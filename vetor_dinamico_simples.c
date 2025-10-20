cd5decbb56b0ffb9a430b222491e3c1935fd4cf6
Kaique <kaiqueboscoaraujo00@gmail.com>
2025-10-20 11:05:58 -0300

    .

--- vetor_dinamico_simples.c
+++ vetor_dinamico_simples.c
@@ -9,14 +9,14 @@ int main(){
     
     printf("\n\nInsira o tamanho da array: ");
     scanf("%d", &size);
-
     array = (int *) malloc(size * sizeof(int));
+    system("clear");
 
     if(array == NULL){
-        printf("Erro ao alocar memória\n");
+        printf(" ~ !! Erro ao alocar memória !! ~\n");
         return 1;
     } else {
-        printf("\n\nMemória alocada com sucesso\n");
+        printf("\n\n ~ Memória alocada com sucesso ~ \n");
     }
 
     printf("\n\nPreencha o conteúdo da array:\n\n");
@@ -24,6 +24,16 @@ int main(){
         printf("Array[%d]: ", i + 1);
         scanf("%d", &array[i]);
     }
+
+    system("clear");
+    printf("\n\n ~ Array preenchida com sucesso ~ \n\n");
+    printf("\n ~ Conteúdo da array ~ \n\n");
+    
+    for(int i = 0; i < size; i++){
+        printf("Array[%d]: %d\n", i + 1, array[i]);
+    }
+
+    printf("\n\n");
 
     free(array);
 
