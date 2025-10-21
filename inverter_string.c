f4d09eb16f029f6bf4f01b5e277ba6d02364739b
Kaique <kaiqueboscoaraujo00@gmail.com>
2025-10-21 12:48:59 -0300

    .

--- inverter_string.c
+++ inverter_string.c
@@ -1,47 +1,47 @@
 // Crie uma função que receba uma string e a inverta usando apenas ponteiros.
-#include <stdio.h>
-#include <stdlib.h>
+// #include <stdio.h>
+// #include <stdlib.h>
 
-void reverseString(char *mainString)
-{
-    char *mainStringPtr = mainString;
-    int stringSize = 0;
+// void reverseString(char *mainString)
+// {
+//     char *mainStringPtr = mainString;
+//     int stringSize = 0;
     
-    while(*mainStringPtr != '\0')
-    {
-        printf("%c", *mainStringPtr);
-        mainStringPtr++, stringSize++;
-    }
+//     while(*mainStringPtr != '\0')
+//     {
+//         printf("%c", *mainStringPtr);
+//         mainStringPtr++, stringSize++;
+//     }
     
-    printf("\n\nstringsize : %d\n\n", stringSize);
-    char *tempStringPtr = (char *)malloc(stringSize * sizeof(char));
+//     printf("\n\nstringsize : %d\n\n", stringSize);
+//     char *tempStringPtr = (char *)malloc(stringSize * sizeof(char));
     
-    int x = stringSize - 1;
+//     int x = stringSize - 1;
 
-    while(*tempStringPtr != '\0')
-    {
-        *tempStringPtr++ = mainString[x--];
-    }
+//     while(*tempStringPtr != '\0')
+//     {
+//         *tempStringPtr++ = mainString[x--];
+//     }
 
 
     
-    for (int i = 0; i < 6; i++)
-    {
-        printf("%c", *(tempStringPtr++));
-    }
+//     for (int i = 0; i < 6; i++)
+//     {
+//         printf("%c", *(tempStringPtr++));
+//     }
     
     
 
-}
+// }
 
-int	main()
-{
-    system("clear");
-    char name1[] = "Kaique";
+// int	main()
+// {
+//     system("clear");
+//     char name1[] = "Kaique";
 
-    printf("\n\n");
-    reverseString(name1);
-    printf("\n\n\n\n");
+//     printf("\n\n");
+//     reverseString(name1);
+//     printf("\n\n\n\n");
 
-    return 0;
-}
+//     return 0;
+// }
